f738a26b9de0f5fee563e3f897d1cc5d08f0c582
Sina Serati <Wintersina@gmail.com>
2016-08-15 21:13:08 -0600

    added splash scene

---
+++ Pause/Assets/Scripts/splashScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class splashScene : MonoBehaviour {
+
+    private float sceneTimer;
+	// Use this for initialization
+	void Start () {
+        sceneTimer = 3f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        sceneTimer -= Time.deltaTime;
+        if (sceneTimer <= 0)
+        {
+            SceneManager.LoadScene("startS4");
+        }
+
+    }
+}
