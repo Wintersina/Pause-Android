8370e8b3c4a59a9238b52784ece250925aa02396
c8095438 <ss7u@mtmail.mtsu.edu>
2016-09-01 16:14:32 -0500

    sync

--- Pause/Assets/Scripts/AdMob.cs
+++ Pause/Assets/Scripts/AdMob.cs
@@ -7,22 +7,36 @@ public class AdMob : MonoBehaviour {
 	public static AdMob Instance{ set; get; }
 
 	private string bannerId= "ca-app-pub-6947554333794592/9092756042";
-	// Use this for initialization
-	void Start () {
+    private float timerBeforeAdsStart;
+    private bool runTimer;
+    // Use this for initialization
+    void Start () {
+        timerBeforeAdsStart = 4f;
+        runTimer = true;    
+	
+    }
 
-		Instance = this;
-		DontDestroyOnLoad (gameObject);
-        #if UNITY_EDITOR
+    void Update()
+    {
+        timerBeforeAdsStart -= Time.deltaTime;
+        if (timerBeforeAdsStart <= 0 && runTimer)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+#if UNITY_EDITOR
             Debug.Log("adds are going to be shown.");
 #elif UNITY_ANDROID
 		    Admob.Instance().initAdmob(bannerId, "non");
 		    Admob.Instance().setTesting(true);
 #endif
 #if UNITY_EDITOR
-        Debug.Log("adds are being shown.");
+            Debug.Log("adds are being shown.");
 #elif UNITY_ANDROID
-		            Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.TOP_CENTER, 0);
+		    Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.TOP_CENTER, 0);
 #endif
+            runTimer = false;
+
+        }
     }
 
 
